1172a16b57ec66a7494108fc68fab5fb1532e0cf
Seema <stalele@ubuntu.(none)>
2014-03-29 14:18:14 -0700

    Find out Div

---
+++ partA/script.js
@@ -0,0 +1,11 @@
+/* use Javascript and jQuery to modify the contents of a page or every page. be creative! you can
+-change colors and fonts (green on back = console mode?)
+-hide or show page components
+-add new components to the page 
+-replace bits of text with other text, for example: https://chrome.google.com/webstore/detail/cloud-to-butt-plus/apmlngnhgbnjpajelfkmabhkfapgnoai?hl=en
+*/
+
+
+
+$('._5pcp').hide();
+
